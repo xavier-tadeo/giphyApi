d166f28bf4a3962d517669232ff779e5b6dc79f4
Tadeo <domenechtadeo@outlook.es>
2022-01-04 19:25:53 +0100

    Create the component header with title

---
+++ src/components/GifExpert/GifExpert.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const GifExpert = () => {
+  return (
+    <div className="gif">
+      <h2 className="gif__title">GifExpert</h2>
+      <hr />
+    </div>
+  );
+};
