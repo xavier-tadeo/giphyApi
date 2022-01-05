7a21105e1d38126b464d2df9ce76c2cf92a35988
Tadeo <domenechtadeo@outlook.es>
2022-01-05 17:00:41 +0100

    Added scss in list of categories in component GifExpert

--- src/components/GifExpert/GifExpert.js
+++ src/components/GifExpert/GifExpert.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AddCategory } from "../AddCategory/AddCategory";
+import "./GifExpert.scss";
 
 export const GifExpert = () => {
   const arrayCategories = ["Doraimon", "Arare", "DragonBall"];
@@ -13,7 +14,7 @@ export const GifExpert = () => {
   return (
     <div className="list">
       <AddCategory setCategories={setCategories} />
-      <ol>
+      <ol className="list__container">
         {categories.map((categorie) => {
           return <li key={categorie}>{categorie}</li>;
         })}
