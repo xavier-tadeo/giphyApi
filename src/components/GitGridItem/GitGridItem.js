21856337c7363a622b1657fcc654a9a09fba228e
Tadeo <domenechtadeo@outlook.es>
2022-01-08 19:59:33 +0100

    Added the className animate css

--- src/components/GitGridItem/GitGridItem.js
+++ src/components/GitGridItem/GitGridItem.js
@@ -3,7 +3,7 @@ import "./GitGridItem.scss";
 
 export const GitGridItem = ({ id, img, title }) => {
   return (
-    <div className="card">
+    <div className="card animate__animated animate__zoomInDown">
       <p className="card__title">{title}</p>
       <img className="card__image" src={img} alt={title} />
     </div>
