a9fc56ce28f0be56f7165c8975b9bae302dcd298
Tadeo <domenechtadeo@outlook.es>
2022-01-10 13:13:09 +0100

    Added in test the find pTag and length to gifs is correct

--- src/components/GifGrid/GitGrid.test.js
+++ src/components/GifGrid/GitGrid.test.js
@@ -1,14 +1,46 @@
 import { GifGrid } from "./GifGrid";
 import { shallow } from "enzyme";
+import { useFetchGif } from "../../hooks/useFetchGif";
+import { GitGridItem } from "../GitGridItem/GitGridItem";
+
+jest.mock("../../hooks/useFetchGif");
 
 describe("Given a GitGrid component", () => {
   describe("When it render", () => {
     test("It should rendered a liTag with name 'categorie' and one divTag", () => {
       const categorie = "Ball dragon";
 
+      useFetchGif.mockReturnValue({
+        data: [],
+        loading: true,
+      });
       const wrapper = shallow(<GifGrid categorie={categorie} />);
 
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe("When it call the function 'useFetchGif'", () => {
+    test("It should show images", () => {
+      const gifs = [
+        {
+          id: "ABC",
+          url: "url",
+          title: "Algo",
+        },
+      ];
+      const categorie = "Ball dragon";
+
+      useFetchGif.mockReturnValue({
+        data: gifs,
+        loading: false,
+      });
+
+      const wrapper = shallow(<GifGrid categorie={categorie} />);
+
+      expect(wrapper).toMatchSnapshot();
+      expect(wrapper.find("p").exists()).toBe(false);
+      expect(wrapper.find(GitGridItem).length).toBe(gifs.length);
     });
   });
 });
