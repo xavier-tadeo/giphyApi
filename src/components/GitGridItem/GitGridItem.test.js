60482e70112d4322ac76db5dcd121b3f33771ee1
Tadeo <domenechtadeo@outlook.es>
2022-01-10 10:43:12 +0100

    Added test for search the className div in GitGridItem component

--- src/components/GitGridItem/GitGridItem.test.js
+++ src/components/GitGridItem/GitGridItem.test.js
@@ -3,15 +3,24 @@ import { GitGridItem } from "./GitGridItem";
 
 describe("Given a GitGridItem component", () => {
   describe("When it render", () => {
+    const image = "url";
+    const title = "Un titulo";
+    const wrapper = shallow(<GitGridItem img={image} title={title} />);
     test("Then should rendered a pTag and imgTag", () => {
-      const image = "url";
-      const title = "Un titulo";
-
-      const wrapper = shallow(<GitGridItem img={image} title={title} />);
-      const pExpect = wrapper.find("p").text();
+      const pExpect = wrapper.find("p").text().trim();
+      const imgExpect = wrapper.find("img");
 
       expect(pExpect).toBe(title);
+      expect(imgExpect.prop("src")).toBe(image);
       expect(wrapper).toMatchSnapshot();
+    });
+
+    test("It should have className 'animate__zoomInDown'", () => {
+      const div = wrapper.find("div");
+
+      const className = div.prop("className");
+
+      expect(className.includes("animate__zoomInDown")).toBe(true);
     });
   });
 });
